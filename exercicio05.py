dcf1b3ee9af0dbbc2a8a7a7083af1fb6fe86da66
Ângelo Vinícius da Silva Santos <81161662+AngeloSant05@users.noreply.github.com>
2024-04-18 00:21:14 +0000

    update exercicio05

--- exercicio05.py
+++ exercicio05.py
@@ -1,1 +1,19 @@
 #5ª questão
+
+ano_atual = int(2024)
+
+cod_empregado = int(input("Digite sua matricula: "))
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+ano_ingresso = int(input("Digite o ano de ingresso na empresa: "))
+idade = ano_atual - ano_nascimento
+tempo_trabalho = ano_atual - ano_ingresso
+
+if (ano_atual - ano_nascimento >= 50 and ano_atual - ano_ingresso) >= 20 \
+  or ano_atual - ano_nascimento >= 55 \
+  or ano_atual - ano_ingresso >= 25:
+  print(f"Você possui {idade} anos e {tempo_trabalho} anos de empresa.\
+  Requerer aposentadoria")
+else:
+  print(f"Você possui {idade} anos e {tempo_trabalho} anos de empresa.\
+Não requerer aposentadoria")
+
